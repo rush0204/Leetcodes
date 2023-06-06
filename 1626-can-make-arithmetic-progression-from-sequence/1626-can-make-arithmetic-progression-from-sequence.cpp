4a760c1d8d071d5c3cdf403e319af71bbbeaba2b
Arushi <83341163+rush0204@users.noreply.github.com>
2023-06-06 12:37:53 +0530

    Time: 9 ms (22.98%), Space: 9.1 MB (66.22%) - LeetHub

--- 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.cpp
+++ 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.cpp
@@ -6,8 +6,9 @@ public:
         if(n==1||n==2||n==0){
             return true;
         }
-       for(int i = 0;i<n-2;i++){
-           if(arr[i]-arr[i+1]!=arr[i+1]-arr[i+2]){
+        int diff = arr[0] -arr[1];
+       for(int i = 0;i<n-1;i++){
+           if(arr[i]-arr[i+1]!=diff){
               return false;
            }
        }
