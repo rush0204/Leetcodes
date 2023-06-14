3ba5da13ff79435912dc0c90dfaed8ff281a682e
Arushi <83341163+rush0204@users.noreply.github.com>
2023-06-14 11:09:25 +0530

    Time: 0 ms (100%), Space: 8.5 MB (31.07%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -13,13 +13,17 @@ class Solution {
 public:
 vector<int> soln;
     vector<int> inorderTraversal(TreeNode* root) {
-        if(root==NULL){
-            return vector<int>();
+      vector<int> nodes;
+        stack<TreeNode*> todo;
+        while (root || !todo.empty()) {
+            while (root) {
+                todo.push(root);
+                root = root -> left;
+            }
+            root = todo.top();
+            todo.pop();
+            nodes.push_back(root -> val);
+            root = root -> right;
         }
-       inorderTraversal(root->left);
-       soln.push_back(root->val);
-       inorderTraversal(root->right);
-    
-return soln;
-    }
+        return nodes;}
 };
