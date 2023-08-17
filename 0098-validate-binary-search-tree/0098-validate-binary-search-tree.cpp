b736330f4e960647c37cac7ab6f930d519704dad
Arushi <83341163+rush0204@users.noreply.github.com>
2023-08-17 22:18:28 +0530

    Time: 8 ms (82.76%), Space: 21.7 MB (30.12%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,41 +11,21 @@
  */
 class Solution {
 public:
-TreeNode* maxm(TreeNode* root){
-    while(root->right){
-        root=root->right;
-    }
-    return root;
-}
-TreeNode* minm(TreeNode* root){
-    while(root->left){
-        root=root->left;
-    }
-    return root;
+bool helper(TreeNode* root, long minm, long maxm){
+    if(root==NULL) return true;
+    if(root->val>minm&&root->val<maxm){
+        if(root->left!=NULL&&root->right!=NULL){
+        return helper(root->left,minm,root->val)&&helper(root->right,root->val,maxm);
+        }
+        else if(root->left) return helper(root->left,minm,root->val);
+        else if(root->right) return helper(root->right,root->val,maxm);
+        else return true;
+    }else return false;
 }
     bool isValidBST(TreeNode* root) {
-        if(root==NULL) return true;
-        if(root->right&&root->left){
-        TreeNode* mini = maxm(root->left);
-        TreeNode* maxi = minm(root->right);
-        if(mini->val<maxi->val&&mini->val<root->val&&maxi->val>root->val){
-            return isValidBST(root->left)&&isValidBST(root->right);
-        }else return false;
-        }
-        else if(root->right){
-            TreeNode* maxi = minm(root->right);
-            if(maxi->val>root->val){
-            return isValidBST(root->left)&&isValidBST(root->right);
-        }else return false;
-        }
-        else if(root->left){
-            TreeNode* mini = maxm(root->left);
-            if(mini->val<root->val){
-            return isValidBST(root->left)&&isValidBST(root->right);
-        }else return false;
-        }
-        else{
-            return true;
-        }
+        long minm = LONG_MIN;
+        long maxm = LONG_MAX;
+        return helper(root,minm,maxm);
+        
     }
 };
