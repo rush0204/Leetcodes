dfcce78b933f7a60061b285be2dcd04d4bdc95c9
Arushi <83341163+rush0204@users.noreply.github.com>
2023-10-19 13:12:21 +0530

    Time: 0 ms (100%), Space: 6.7 MB (29.19%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,34 +1,72 @@
+// class Solution {
+// public:
+//     bool backspaceCompare(string s, string t) {
+//         int h = 0;
+//         string s1 = ""; 
+//         for(int i = s.length()-1;i>-1;i--){
+//             if(s[i]=='#'){
+//                 h++;
+//             }
+//             if(h==0){
+//                 s1 = s[i] + s1;
+//             }
+//             else{
+//                 if(s[i]!='#'){
+//                     h--;
+//                 }
+//             }
+//         }
+//         int h1 = 0;
+//         string t1 = "";
+//          for(int i = t.length()-1;i>-1;i--){
+//             if(t[i]='#'){
+//                 h1++;
+//             }
+//             if(h1==0){
+//                 t1 = t[i] + t1;
+//             }
+//             else{
+//                 if(t[i]!='#'){
+//                     h1--;
+//                 }
+//             }
+//         }
+//         cout<<s1<<endl;
+//         cout<<"1 is "<<t1<<endl;
+//         if(s1==t1) return 1; 
+//         else return 0;
+//     }
+// };
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> st1;
-        stack<char> st2;
-        for(int i = 0;i<s.length();i++){
-            if(s[i]!='#'){
-                st1.push(s[i]);
+        int h = 0;
+        string s1 = "";
+        string t1 = "";
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == '#') {
+                h++;
             }
-            if(s[i]=='#'){
-                if(!st1.empty()) st1.pop();
-            }
-        }
-          for(int i = 0;i<t.length();i++){
-            if(t[i]!='#'){
-                st2.push(t[i]);
-            }
-            if(t[i]=='#'){
-                 if(!st2.empty())  st2.pop();
+            if (h == 0) {
+                s1 = s[i] + s1;
+            } else if (s[i] != '#') {
+                h--;
             }
         }
-        bool ans = true;
-        while(!st1.empty()&&!st2.empty()){
-            if(st1.top()==st2.top()){
-                st1.pop();
-                st2.pop();
+
+        int h1 = 0;
+        for (int i = t.length() - 1; i >= 0; i--) {
+            if (t[i] == '#') {
+                h1++;
             }
-            else { cout<<st1.top()<<" "<<st2.top(); ans = false; break;}
+            if (h1 == 0) {
+                t1 = t[i] + t1;
+            } else if (t[i] != '#') {
+                h1--;
+            }
         }
-        if(!st1.empty()&&st2.empty()) return false;
-          if(st1.empty()&&!st2.empty()) return false;
-        return ans;
+
+        return s1 == t1;
     }
 };
